0643eb84fa7886fabede434a1475dda6da7e5c4b
BSS-SUPPORT\NWUUser <bashir.ahmed@nwu.ac.za>
2019-04-17 16:19:42 +0200

    dict eg's

--- hello.py
+++ hello.py
@@ -10,3 +10,14 @@ print("contents:")
 print(contents)
 
 print("\nwe printed the index.html file contents")
+
+#simple dictionary
+months = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April"
+}
+ 
+print(months.get(2))
+print(months[2])
