22016ea8fbff4483769d17d5bbcfc00f18dffbd7
BSS-SUPPORT\NWUUser <bashir.ahmed@nwu.ac.za>
2019-04-17 12:59:37 +0200

    layout changes

--- hello.py
+++ hello.py
@@ -2,11 +2,11 @@
 
 print("hello kitty")
 
-
 var_file = "index.html"
 with open(var_file) as file_in:
     contents = file_in.readlines()
 
+print("contents:")
 print(contents)
 
 print("\nwe printed the index.html file contents")
